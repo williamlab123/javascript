8fc1d5825cb06cbefc52544e8a2dbc55894e7f15
William Pedro <54008790+williamlab123@users.noreply.github.com>
2022-11-17 17:20:17 -0300

    Add files via upload

---
+++ forcount.js
@@ -0,0 +1,11 @@
+var contagem = 0
+
+var rand = Math.floor(Math.random() * 1000 + 1) 
+
+for(rand; rand>0;rand--)
+{
+  contagem += 1
+
+}
+
+console.log(contagem)
--- var1.js
+++ var1.js
@@ -22,5 +22,14 @@ function teste()
 }
 
 teste()
-console.log(local)
+//console.log(local)
+//neste caso, a variavel local nao pode ser chamnada fora do bloco
+//pois uma variavel que esta dentro de uma funçao
+//so pode ser chamada dentro dessa funçao, esse é o escopo dela
+//quando voce cria uma variavel fora de uma funçao
+//essa variavel é global ou seja, ela estara
+//disponivel para todo o codigo
+//as variaveis so tem 2 escopos possiveis
+//escopo global, e escopo local, escopo de funçao
+
 
